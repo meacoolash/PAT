a24bf95a4921de0e283dda3a84a005f20cd45414
Miki Stec <mikulas.stec@nordlicht-it.com>
2017-05-17 16:28:49 +0200

    asdf

---
+++ journal.js
@@ -0,0 +1,38 @@
+---------------------------------
+PATRIUM 
+---------------------------------
+
+---------------------------------
+3 may 2017 
+Odprezentovaný layout 
+	-menej červenej, zapracovať line ako na vizitke 
+	-inak ok. 
+	-môže byť jednostránková landingpage. 
+
+MIX: 
+	Opýtať Referencie !! 
+	darinaoravcova.sk 
+	Komunikácia s Mrak.sk 
+		presunutie hostingu 
+		prepočítať platbu, navrhnutá cena 4,50/ mesiac
+		vystaviť faktúru na min. 150E / záloha 
+	Nastavenie emailu 
+	Nebezpečná stránka?? 
+
+Osobné: 
+	-o 6 týždňov rodí
+	-má črevnú chrípku
+	-Jašek sa dnes porozpráva s ekonómkou a dá vedieť. 
+
+
+---------------------------------
+
+
+
+
+
+24 february	2017
+Stretnutie, prevzaté podklady. 
+Dohodnutá suma 300. 
+Bez zálohy. 
+
